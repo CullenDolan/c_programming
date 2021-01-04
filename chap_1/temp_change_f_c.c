54c7cac2f1cb652edcd61d795072e57db1f165ac
cullen <cullen.dolan@gmail.com>
2021-01-04 14:04:38 -0600

    more temp changes

--- chap_1/temp_change_f_c.c
+++ chap_1/temp_change_f_c.c
@@ -4,23 +4,24 @@
 
 #include <stdio.h>
 
+#define LOWER  0
+#define UPPER 300
+#define STEP 20
+
 main()
 {
 	// C = (5/9)(F-32)
 	float fahr, celcius;
 	int lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-	fahr = lower;
+	fahr = LOWER;
 
 	printf("Fahr \t Celcius\n");
 
-	while(fahr <= upper){
+	while(fahr <= UPPER){
 		celcius = (5.0/9.0) * (fahr - 32.0);
 		printf("%3.0f \t %0.1f\n", fahr, celcius);
-		fahr = fahr + step;
+		fahr = fahr + STEP;
 	}
 
 }
