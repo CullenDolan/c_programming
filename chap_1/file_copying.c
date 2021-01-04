a2c1012ac978292a98950b8cfc42102c486f032c
cullen <cullen.dolan@gmail.com>
2021-01-04 14:27:50 -0600

    file copying prgram

---
+++ chap_1/file_copying.c
@@ -0,0 +1,13 @@
+// take input from user and return it
+
+#include <stdio.h>
+
+main(){
+	int c;
+
+	printf("Type a letter: ");
+	while ((c = getchar())!= EOF){
+		printf("%d\n", EOF);
+		putchar(c);
+	}
+}
